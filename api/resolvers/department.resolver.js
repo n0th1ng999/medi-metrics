44aabbc878821ba759623a4d5caf885a9c759c6c
Francisco Gameiro <gameirofrancisco6@gmail.com>
2025-01-27 12:41:05 +0000

    ci: :art: Estrututação do Department

--- api/resolvers/department.resolver.js
+++ api/resolvers/department.resolver.js
@@ -1,5 +1,45 @@
 import { Department, Nurse, Bed, Patient } from "../database/index.js";
 
-const DepartmentResolver = {}
+const DepartmentResolver = {
+    Query: {
+
+    },
+    Mutation: {
+        /**
+		 * * Done ✅
+		 * @param {*} _
+		 * @param {{name:String}} input
+		 */
+		createDepartment: async (_, { name }) => {
+			try {
+				const newDepartment = new Department({ name });
+
+				await newDepartment.save();
+
+				return { success: true, message: "Department created successfully" };
+			} catch (error) {
+				return { success: false, message: error.message };
+			}
+		},
+		/**
+		 * * Done ✅
+		 * @param {*} _
+		 * @param {{id:String}} input
+		 */
+		deleteDepartment: async (_, { id }) => {
+			try {
+				const deletedDepartment = await Department.findByIdAndDelete(id);
+
+				if (!deletedDepartment) {
+					return { success: false, message: "Department not found" };
+				}
+
+				return { success: true, message: "Deletion Successful" };
+			} catch (error) {
+				return { success: false, message: `Error: ${error}` };
+			}
+		},
+    }
+}
 
 export default DepartmentResolver
